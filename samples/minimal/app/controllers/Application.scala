257959e6022cbcde3c68680d190f3fd2fbfb0bae
Martin Blech <martinblech@gmail.com>
2012-12-11 15:12:21 -0300

    updated sample app with new mapping DSL syntax

--- samples/minimal/app/controllers/Application.scala
+++ samples/minimal/app/controllers/Application.scala
@@ -7,10 +7,10 @@ import mimerender._
 
 object Application extends Controller {
   val m = mapping(
-    "text/html" -: { s: String => views.html.index(s) },
-    "application/xml" -: { s: String => <message>{s}</message> },
-    "application/json" -: { s: String => Json.obj("message" -> s) },
-    "text/plain" -: identity[String]_
+    "text/html" -> { s: String => views.html.index(s) },
+    "application/xml" -> { s: String => <message>{s}</message> },
+    "application/json" -> { s: String => Json.obj("message" -> s) },
+    "text/plain" -> identity[String]_
   )
   
   def index = Action { implicit request =>
