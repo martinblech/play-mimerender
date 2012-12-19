45779e1129bdd6a1418c446eb6b5a32d762d5531
Martin Blech <martinblech@gmail.com>
2012-12-19 13:04:05 -0300

    queryStringOverride in sample app

--- samples/minimal/app/controllers/Application.scala
+++ samples/minimal/app/controllers/Application.scala
@@ -18,9 +18,9 @@ object Application extends Controller {
     "text/html" -> { s: String => views.html.index(s) },
     "application/xml" -> { s: String => <message>{s}</message> },
     "application/json" -> jsonTransform _,
-    "text/javascript" -> jsonpTransform _,
+    "application/javascript" -> jsonpTransform _,
     "text/plain" -> identity[String]_
-  )
+  ) queryStringOverride "format"
   
   def index = Action { implicit request =>
     m.status(200)("Hello, world!")
